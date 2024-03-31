2552013d51598d28a14e42820ab9376ee58d8680
HARSHAN A M <harshanmathi06@gmail.com>
2024-03-31 22:05:55 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config.database import db
-
-collection=db['users']
-
-task=db.task.find_one({'task_id':'t11'})
-task_assigned_to_role=db.users.find_one({'user_id':task['assigned_to']})['role']
-print(task_assigned_to_role)
